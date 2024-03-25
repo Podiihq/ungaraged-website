86e6c015187da4695ac2a90d1628a6183748fa79
Parlgy <parlg4776@gmail.com>
2024-03-26 01:17:00 +0300

    first commit

---
+++ src/components/Icons.tsx
@@ -0,0 +1,42 @@
+
+export const SearchIcon = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4 lg:w-6 lg:h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+        </div>
+    )
+}
+
+
+export const ArrowForward = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4 lg:w-6 lg:h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+            </svg>
+        </div>
+    )
+}
+
+
+export const Mouse = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-tabler icons-tabler-outline icon-tabler-mouse">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6 3m0 4a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-4a4 4 0 0 1 -4 -4z" />
+                <path d="M12 7l0 4" />
+            </svg>
+        </div>
+    )
+}
+
+
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,64 @@
+import Logo from "../assets/Logo/Logo.svg"
+import { ArrowForward, Mouse, SearchIcon } from "../components/Icons"
+import Hero from "../assets/images/hero/Hero.png"
+import HeroMobile from "../assets/images/hero/HeroMobile.png"
+
+
+const HomePage = () => {
+    return (
+        <div className="bg-black text-white">
+            <NavBar />
+            <section className="relative">
+                <div className="relative">
+                    <img src={Hero} alt="" className="object-cover w-full -z-10 hidden lg:block" />
+                    <img src={HeroMobile} alt="" className="object-cover w-full -z-10 lg:hidden" />
+                    <div className="absolute left-0 bottom-[40%] lg:left-[5%] lg:bottom-[40%] md:px-24 lg:px-8 italic text-white">
+                        <div className="z-50 lg:w-1/2 mt-32 px-4">
+                            <p className="font-bold ">Lets Tune Your Car</p>
+                            <p className="text-[44px] lg:text-[96px] font-[900] uppercase leading-none">Speed, <span className="text-[#FE5C43]">Power </span>Performance.</p>
+                            <p className="lg:px-0 text-sm lg:text-base mt-4">Experience the exhilaration of driving beyond limits, where power, speed, and performance converge to redefine what's possible on the road, unleashing the full potential of your vehicle and leaving ordinary behind.</p>
+                        </div>
+                        <div className="flex lg:mt-8 px-4 mt-4">
+                            <a href="#" className="relative">
+                                <span className="absolute top-0 left-0 mt-1 ml-1 lg:mt-1.5 lg:ml-1.5 h-full w-full bg-white -skew-x-[15deg] transform"></span>
+                                <span className="-skew-x-[15deg] transform flex items-center gap-2 bg-[#FE5C43] fold-bold relative h-full w-full px-4 py-2 lg:px-6 lg:py-3 text-sm lg:text-base font-semibold text-white transition duration-100">
+                                    <p className="skew-x-[15deg]">Get in Touch</p>
+                                    <div className="skew-x-[15deg]">
+                                        <ArrowForward />
+                                    </div>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex justify-center absolute bottom-20 left-[50%]">
+                        <Mouse />
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default HomePage
+
+
+export const NavBar = () => {
+    return (
+        <div className="bg-black text-white">
+            <nav className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8">
+                <div className="flex justify-between items-center">
+                    <img src={Logo} alt="" className="w-32 lg:w-auto" />
+                    <div className="lg:flex items-center gap-6 text-sm hidden">
+                        <a href="">Services</a>
+                        <a href="">About Us</a>
+                        <a href="">Contact Us</a>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <SearchIcon />
+                        <p className="text-sm hidden lg:blok">Search your car</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
