70545e70e444b8b5da75ca9c87d0c6b9d09a852c
Parlgy <parlg4776@gmail.com>
2024-03-26 02:48:10 +0300

    created navbar component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
-import Logo from "../assets/Logo/Logo.svg"
-import { ArrowForward, SearchIcon } from "../components/Icons"
+import { ArrowForward } from "../components/Icons"
 import Hero from "../assets/images/hero/Hero.png"
 import HeroMobile from "../assets/images/hero/HeroMobile.png"
 import MouseSvg from "../assets/images/icons/mouse.svg"
 import HeroVideo from "../assets/videos/vid-2.mp4"
+import { NavBar } from "../components/NavBar"
 
 
 const HomePage = () => {
@@ -58,23 +58,3 @@ const HomePage = () => {
 export default HomePage
 
 
-export const NavBar = () => {
-    return (
-        <div className="bg-black text-white">
-            <nav className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8">
-                <div className="flex justify-between items-center">
-                    <img src={Logo} alt="" className="w-32 lg:w-auto" />
-                    <div className="lg:flex items-center gap-6 text-sm hidden">
-                        <a href="">Services</a>
-                        <a href="">About Us</a>
-                        <a href="">Contact Us</a>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <SearchIcon />
-                        <p className="text-sm hidden md:block">Search your car</p>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    )
-}
