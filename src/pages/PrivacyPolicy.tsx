a21799aee6004c78ffee47696f9f8b289b1cf2aa
Parlgy <parlg4776@gmail.com>
2026-05-06 23:38:47 +0300

    fix base url bug

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect, ReactNode } from "react";
 import Logo from "../assets/Logo/Logo.svg"
 
 
